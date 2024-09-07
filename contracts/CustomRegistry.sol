13ab433c22e645770205d251bd4bd7cb2058edce
PraneshASP <praneshas2000@gmail.com>
2024-09-07 11:48:19 +0530

    feat: more tests

--- contracts/CustomRegistry.sol
+++ contracts/CustomRegistry.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.22;
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { OApp, Origin } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
 import { ERC6551Registry } from "./vendor/ERC6551Registry.sol";
-
 contract CustomRegistry is ERC6551Registry, OApp {
     constructor(address _endpoint, address _owner) OApp(_endpoint, _owner) Ownable(msg.sender) {}
     
@@ -20,10 +19,12 @@ contract CustomRegistry is ERC6551Registry, OApp {
         address sender = address(uint160(uint256(_origin.sender)));
         (address implementation, bytes32 salt, uint256 chainId, address tokenContract, uint256 tokenId) = 
             abi.decode(_message, (address, bytes32, uint256, address, uint256));
-
-        require(sender == tokenContract, "INVALID_SENDER");
         
         address _account = this.createAccount(implementation, salt, chainId, tokenContract, tokenId);
-        paymentModules[keccak256(abi.encode(chainId, tokenId, sender))] = _account;
+        paymentModules[keccak256(abi.encode(chainId, tokenId, tokenContract))] = _account;
     }
+    function getPaymentModule(uint256 chainId, uint256 tokenId, address tokenContract) public view returns (address) {
+        return paymentModules[keccak256(abi.encode(chainId, tokenId, tokenContract))];
+    }
+
 }
--- test/foundry/CreatorNFTTest.t.sol
+++ test/foundry/CreatorNFTTest.t.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.22;
 
 import { CreatorNFT } from "../../contracts/CreatorNFT.sol";
+import { CustomRegistry } from "../../contracts/CustomRegistry.sol";
 import { IOAppOptionsType3, EnforcedOptionParam } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
 import { OptionsBuilder } from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "forge-std/console.sol";
 import { TestHelperOz5 } from "@layerzerolabs/test-devtools-evm-foundry/contracts/TestHelperOz5.sol";
 import { IERC6551Registry } from "../../contracts/interfaces/IERC6551Registry.sol";
+import { AddressCast } from "@layerzerolabs/lz-evm-protocol-v2/contracts/libs/AddressCast.sol";
+import { Origin } from "@layerzerolabs/oapp-evm/contracts/oapp/OApp.sol";
+import { PaymentModel } from "contracts/PaymentModel.sol";
 
 contract CreatorNFTTest is TestHelperOz5 {
     using OptionsBuilder for bytes;
@@ -16,14 +20,14 @@ contract CreatorNFTTest is TestHelperOz5 {
     uint32 private bEid = 2;
 
     CreatorNFT private aCreatorNFT;
-    CreatorNFT private bCreatorNFT;
+    CustomRegistry private aRegistry;
+    CustomRegistry private bRegistry;
 
     address private userA = address(0x1);
     address private userB = address(0x2);
     uint256 private initialBalance = 100 ether;
 
-    address private mockRegistry = address(0x3);
-    address private mockPaymentModuleImplementation = address(0x4);
+    address private paymentModuleImpl;
 
     function setUp() public virtual override {
         vm.deal(userA, 1000 ether);
@@ -32,66 +36,164 @@ contract CreatorNFTTest is TestHelperOz5 {
         super.setUp();
         setUpEndpoints(2, LibraryType.UltraLightNode);
 
-        aCreatorNFT = new CreatorNFT(address(endpoints[aEid]), address(this), mockRegistry, mockPaymentModuleImplementation);
-        bCreatorNFT = new CreatorNFT(address(endpoints[bEid]), address(this), mockRegistry, mockPaymentModuleImplementation);
+        // Deploy CustomRegistry on both chains
+        aRegistry = new CustomRegistry(address(endpoints[aEid]), address(this));
+        bRegistry = new CustomRegistry(address(endpoints[bEid]), address(this));
 
-        address[] memory oapps = new address[](2);
+        // Deploy the payment module
+        paymentModuleImpl = address(new PaymentModel());
+
+        // Deploy CreatorNFT only on chain A
+        aCreatorNFT = new CreatorNFT(address(endpoints[aEid]), address(this), address(aRegistry), paymentModuleImpl);
+
+        // Wire OApps
+        address[] memory oapps = new address[](3);
         oapps[0] = address(aCreatorNFT);
-        oapps[1] = address(bCreatorNFT);
+        oapps[1] = address(aRegistry);
+        oapps[2] = address(bRegistry);
         this.wireOApps(oapps);
+
+        // Set peers for CreatorNFT and CustomRegistry
+        aCreatorNFT.setPeer(bEid, AddressCast.toBytes32(address(bRegistry)));
+        aRegistry.setPeer(bEid, AddressCast.toBytes32(address(bRegistry)));
+        bRegistry.setPeer(aEid, AddressCast.toBytes32(address(aRegistry)));
     }
 
-    function test_constructor() public {
-        assertEq(aCreatorNFT.owner(), address(this));
-        assertEq(bCreatorNFT.owner(), address(this));
-
-        assertEq(address(aCreatorNFT.endpoint()), address(endpoints[aEid]));
-        assertEq(address(bCreatorNFT.endpoint()), address(endpoints[bEid]));
-    }
-
-    function test_quoteDeployPaymentModule() public {
-        uint256 tokenId = aCreatorNFT.mint("ipfs://test");
-        vm.prank(userA);
-        (uint256 nativeFee, uint256 zroFee) = aCreatorNFT.getDeploymentFees(tokenId, bEid, address(0x1234));
-
-        assertGt(nativeFee, 0, "Native fee should be greater than 0");
-        assertEq(zroFee, 0, "ZRO fee should be 0");
-    }
-       function test_deployPaymentModuleCrossChain() public {
+    function test_crossChainDeployPaymentModule() public {
+        // Mint an NFT on chain A
         vm.prank(userA);
         uint256 tokenId = aCreatorNFT.mint("ipfs://test");
-        vm.prank(userA);
-        
-        (uint256 nativeFee, ) = aCreatorNFT.getDeploymentFees(tokenId, bEid, address(0x1234));
 
+        // Get deployment fees
+        (uint256 nativeFee, ) = aCreatorNFT.getDeploymentFees(tokenId, bEid, paymentModuleImpl);
+
+        // Deploy payment module cross-chain
+        vm.prank(userA);
         vm.expectEmit(true, true, true, true);
         emit CreatorNFT.PaymentModuleDeployed(tokenId, bEid, address(0));
+        aCreatorNFT.deployPaymentModule{value: nativeFee}(tokenId, bEid, paymentModuleImpl);
 
-        vm.prank(userA);
-        aCreatorNFT.deployPaymentModule{value: nativeFee}(tokenId, bEid, address(0x1234));
-
-        // Simulate the cross-chain message being received
+        // Simulate the cross-chain message being received and processed on chain B
         bytes memory payload = abi.encode(
-            aCreatorNFT.DEPLOY_PAYMENT_MODULE(),
-            mockRegistry,
-            abi.encode(address(0x1234), keccak256(abi.encode(address(aCreatorNFT), tokenId, bEid)), bEid, address(aCreatorNFT), tokenId)
+            paymentModuleImpl,
+            keccak256(abi.encode(address(aCreatorNFT), tokenId, bEid)),
+            bEid,
+            address(aCreatorNFT),
+            tokenId
         );
 
-        // vm.expectEmit(true, true, true, true);
-        // emit CreatorNFT.PaymentModuleDeployed(tokenId, aEid, address(0x5678));
+        // Create the Origin struct
+         Origin memory origin = Origin({
+            srcEid: aEid,
+            sender: AddressCast.toBytes32(address(aRegistry)),
+            nonce: 1 // You might want to use an actual nonce here
+        });
 
-    //     vm.mockCall(
-    //         mockRegistry,
-    //         abi.encodeWithSelector(IERC6551Registry.createAccount.selector),
-    //         abi.encode(address(0x5678))
-    //     );
+        vm.prank(address(endpoints[bEid]));
+        bRegistry.lzReceive(
+            origin,
+            bytes32(0), // _guid
+            payload,
+            address(0), // _executor
+            "" // _extraData
+        );
 
-    //    // bCreatorNFT.lzReceive(aEid, abi.encodePacked(address(aCreatorNFT)), 0, payload, "", 0);
 
-    //     assertEq(bCreatorNFT.paymentModules(tokenId, aEid), address(0x5678), "Payment module should be deployed on chain B");
+        // Verify the payment module was deployed on chain B
+        bytes32 salt = keccak256(abi.encode(address(aCreatorNFT), tokenId, bEid));
+      
+
+        address expectedPaymentModule = bRegistry.getPaymentModule(bEid, tokenId, address(aCreatorNFT));
+        assertTrue(expectedPaymentModule != address(0), "Payment module should be deployed on chain B");
+
+        // Verify the payment module details
+        (uint256 chainId, address tokenContract, uint256 deployedTokenId) = PaymentModel(payable(expectedPaymentModule)).token();
+        assertEq(chainId, bEid, "Incorrect chain ID");
+        assertEq(tokenContract, address(aCreatorNFT), "Incorrect token contract address");
+        assertEq(deployedTokenId, tokenId, "Incorrect token ID");
     }
 
-    function onERC721Received(address,address,uint256,bytes calldata) external returns (bytes4) {
-        return 0x150b7a02;
+       function test_createTier() public {
+        // Mint an NFT and deploy payment module
+        vm.prank(userA);
+        uint256 tokenId = aCreatorNFT.mint("ipfs://test");
+        (uint256 nativeFee, ) = aCreatorNFT.getDeploymentFees(tokenId, bEid, paymentModuleImpl);
+        vm.prank(userA);
+        aCreatorNFT.deployPaymentModule{value: nativeFee}(tokenId, bEid, paymentModuleImpl);
+
+        // Simulate cross-chain message processing
+        bytes memory payload = abi.encode(
+            paymentModuleImpl,
+            keccak256(abi.encode(address(aCreatorNFT), tokenId, bEid)),
+            bEid,
+            address(aCreatorNFT),
+            tokenId
+        );
+        Origin memory origin = Origin({
+            srcEid: aEid,
+            sender: AddressCast.toBytes32(address(aRegistry)),
+            nonce: 1
+        });
+        vm.prank(address(endpoints[bEid]));
+        bRegistry.lzReceive(origin, bytes32(0), payload, address(0), "");
+
+        // Get the deployed payment module address
+        address paymentModule = bRegistry.getPaymentModule(bEid, tokenId, address(aCreatorNFT));
+
+        // Create a tier
+        uint256 price = 0.1 ether;
+        uint256 duration = 30 days;
+        vm.prank(userA);
+        PaymentModel(payable(paymentModule)).createTier(price, duration);
+
+        // Verify tier creation
+        (uint256 tierPrice, uint256 tierDuration, bool isActive) = PaymentModel(payable(paymentModule)).subscriptionTiers(1);
+        assertEq(tierPrice, price, "Incorrect tier price");
+        assertEq(tierDuration, duration, "Incorrect tier duration");
+        assertTrue(isActive, "Tier should be active");
+    }
+
+    function test_subscribe() public {
+        // Set up NFT, payment module, and tier (reusing code from test_createTier)
+        vm.prank(userA);
+        uint256 tokenId = aCreatorNFT.mint("ipfs://test");
+        (uint256 nativeFee, ) = aCreatorNFT.getDeploymentFees(tokenId, bEid, paymentModuleImpl);
+        vm.prank(userA);
+        aCreatorNFT.deployPaymentModule{value: nativeFee}(tokenId, bEid, paymentModuleImpl);
+
+        // Simulate cross-chain message processing
+        bytes memory payload = abi.encode(
+            paymentModuleImpl,
+            keccak256(abi.encode(address(aCreatorNFT), tokenId, bEid)),
+            bEid,
+            address(aCreatorNFT),
+            tokenId
+        );
+        Origin memory origin = Origin({
+            srcEid: aEid,
+            sender: AddressCast.toBytes32(address(aRegistry)),
+            nonce: 1
+        });
+        vm.prank(address(endpoints[bEid]));
+        bRegistry.lzReceive(origin, bytes32(0), payload, address(0), "");
+
+        address paymentModule = bRegistry.getPaymentModule(bEid, tokenId, address(aCreatorNFT));
+        vm.prank(userA);
+        PaymentModel(payable(paymentModule)).createTier(0.1 ether, 30 days);
+
+        // Subscribe to the tier
+        vm.prank(userB);
+        PaymentModel(payable(paymentModule)).subscribe{value: 0.1 ether}(1);
+
+        // Verify subscription
+        assertTrue(PaymentModel(payable(paymentModule)).isSubscribed(userB), "User B should be subscribed");
+
+        (uint256 subTierId, uint256 startTime, uint256 endTime) = PaymentModel(payable(paymentModule)).getSubscriptionDetails(userB);
+        assertEq(subTierId, 1, "Incorrect subscription tier ID");
+        assertEq(endTime - startTime, 30 days, "Incorrect subscription duration");
+    }
+
+    function onERC721Received(address,address,uint256,bytes calldata) external pure returns (bytes4) {
+        return this.onERC721Received.selector;
     }
 }
